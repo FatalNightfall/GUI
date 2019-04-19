2c3af73adbe5168ec379762f9477b6c1033e690c
FatalNightfall <jobnotfound@gmail.com>
2019-04-19 23:38:19 +0900

    learning

---
+++ venv/test.py
@@ -0,0 +1,43 @@
+# -*- coding: cp932 -*-
+
+a = 5
+print(a)
+print(float(a))
+print(a)
+
+
+a = float(a)
+print(a)
+
+print(a + a)
+print(a + 5)
+
+b = 5
+
+print(a + a)
+print(a + float(b))
+int(a +float(b))
+
+
+a = 5
+b = 2.7
+c = 4.1
+
+
+d = b * c
+
+int(d)
+print(d + int(a))
+
+
+word = "abcdef"
+
+print(word[-3])
+
+squares = [1, 4, 9, 16, 25]
+
+print(squares[2])
+
+print(squares[:2])
+
+print(squares[:-1])
